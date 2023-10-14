0fab1fa1caef85cb33ad3174449b4eaca694fcdd
0xsoi <sinclairolumide15@gmail.com>
2023-10-14 23:18:50 +0100

    third commit

--- script.sh
+++ script.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
 # PROVISIONING THE VAGRANT MACHINE
-vagrant up
+#vagrant up master
+#vagrant up slave
 
-# CREATING USER ON MASTER NODE
-vagrant ssh master -c "sudo useradd -m altschool"
+# USEER MANAGEMENT
+#vagrant ssh master -c "sudo useradd -m altschool"
 # GIVING USER ROOT PRIVILEGES
-vagrant ssh master -c "sudo usermod -aG sudo altschool"
+#vagrant ssh master -c "sudo usermod -aG sudo altschool"
 
-echo "Created user "altschool" with root privileges on the master node"
+#echo "Created user "altschool" with root privileges on the master node"
 
-# Creating passwordless access from master node to slave node
-vagrant ssh master -c "ssh-keygen -t ed25519 -N '' /home/altschool/.ssh/id_ed25519"
+# INTER NODE COMMUNICATION
+#generate_ssh_key() {
+#    vagrant ssh master -c "sudo su - altschool -c 'mkdir -p /home/altschool/.ssh && ssh-keygen -t ed25519 -N \"0000\" -f /home/altschool/.ssh/id_ed25519'"
+#    vagrant ssh slave -c "sudo mkdir -p /home/altschool/.ssh"
+#    vagrant ssh slave -c "sudo bash -c 'echo \"$(vagrant ssh master -c 'cat /home/altschool/.ssh/id_ed25519.pub')\" >> /home/altschool/.ssh/authorized_keys'"
+#}
 
-vagrant ssh slave -c "sudo mkdir -p /home/altschool/.ssh"
+#generate_ssh_key
 
-vagrant ssh slave -c "sudo bash -c 'echo \"`vagrant ssh master -c 'cat /home/altschool/.ssh/id_ed25519.pub'`"
+#echo "Copied ssh keys successfully"
 
-echo "Copied ssh keys successfully"
-
-# COPYING FILES FROM MASTER TO SLAVE
-vagrant ssh master -c "sudo cp -R /mnt/altschool /mnt/altschool/slave"
+# DATA MANAGEMENT AND TRANSFER
+vagrant ssh master -c "[ ! -d /mnt/altschool ] && sudo mkdir -p /mnt/altschool"
+vagrant ssh master -c "sudo chown altschool:altschool /mnt/altschool && sudo chmod u+r /mnt/altschool"
+vagrant ssh master -c "sudo apt-get install rsync sshpass -y"
+vagrant ssh master -c "sudo rsync -avz --rsync-path='mkdir -p /mnt/altschool/slave && rsync' /mnt/altschool/ altschool@192.168.56.3:/mnt/altschool/slave"
 
 echo "Copied files successfully"
 
-# DEPLOYING THE LAMP STACK 
-vagrant ssh master -c "sudo apt-get update && sudo apt-get install -y apache2 mysql-server php libapache2-mod-php php-mysql"
-vagrant ssh slave -c "sudo apt-get update && sudo apt-get install -y apache2 mysql-server php libapache2-mod-php php-mysql"
+# PROCESS MONITORING
+vagrant ssh master -c "top"
 
-echo "Successfully insalled the LAMP stack on both nodes"
+# DEPLOYING THE LAMP STACK 
+#vagrant ssh master -c "sudo apt-get update && sudo apt-get install -y apache2 mysql-server php libapache2-mod-php php-mysql"
+#vagrant ssh slave -c "sudo apt-get update && sudo apt-get install -y apache2 mysql-server php libapache2-mod-php php-mysql"
+
+#echo "Successfully insalled the LAMP stack on both nodes"
 
 # CONFIGURING APACHE TO START ON BOOT
-vagrant ssh master -c "sudo systemctl start apache2"
-vagrant ssh slave -c "sudo systemctl start apache2"
+#vagrant ssh master -c "sudo systemctl start apache2"
+#vagrant ssh slave -c "sudo systemctl start apache2"
 
 # SECURING MY SQL INSTALLATION
-vagrant ssh master -c "sudo mysql_secure_installation"
+#vagrant ssh master -c "sudo mysql_secure_installation"
 
 # VALIDATING PHP
-vagrant ssh master -c "echo "<?phpinfo(): ?>" | sudo tee /var/www/index/info.php"
+#vagrant ssh master -c "echo "<?phpinfo(); ?>" | sudo tee /var/www/index/info.php"
